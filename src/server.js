7918ed5fe7e075abe465b3a44021bddd9bf3f311
Aecio Silva <aeciofcs@gmail.com>
2020-06-04 17:20:39 -0300

    Instalando e aplicando o nunjucks no projeto

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@
    2 - Instalar o modulo Nodemon para reiniciar o 
        Server, automaticamente, sempre que for feito
        uma alteração em src/server.js
+
+   3 - Instalar o modulo nunjucks, criação de templates
+       HTML inteligentes
 */
  
 const Express = require("express")
@@ -12,18 +15,32 @@ const server = Express()
 //Configurar pasta publica
 server.use(Express.static("public"))
 
+//Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 //Configurar caminhos da minha aplicação
 // Pagina Inicial
 
 //req: é a requisição do browser
 //res: é a resposta à requisição
 server.get("/", (req, res) => {
-   res.sendFile(__dirname + "/views/index.html")
+   return res.render("index.html", {
+       title: "Seu marketplace de coleta de resíduos"
+   })
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
  })
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
 
 //Ligar o Servidor
 server.listen(3000)
