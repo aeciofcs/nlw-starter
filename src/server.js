8f15a859f504844679ea689c4675bae11b99d9de
Aecio Silva <aeciofcs@gmail.com>
2020-06-05 17:31:15 -0300

    Instalando o banco de dados e criando crud.

--- src/server.js
+++ src/server.js
@@ -8,12 +8,18 @@
    3 - Instalar o modulo nunjucks, criação de templates
        HTML inteligentes
 */
- 
+
 const Express = require("express")
 const server = Express()
 
+//Pegar o banco de dados
+const db = require("./database/db")
+
 //Configurar pasta publica
 server.use(Express.static("public"))
+
+//Habilitar o uso do req.body na nossa aplicação
+server.use(Express.urlencoded({ extended: true }))
 
 //Utilizando template engine
 const nunjucks = require("nunjucks")
@@ -29,17 +35,68 @@ nunjucks.configure("src/views", {
 //req: é a requisição do browser
 //res: é a resposta à requisição
 server.get("/", (req, res) => {
-   return res.render("index.html", {
-       title: "Seu marketplace de coleta de resíduos"
-   })
+    return res.render("index.html", {
+        title: "Seu marketplace de coleta de resíduos"
+    })
 })
 
 server.get("/create-point", (req, res) => {
+    // req.query: query Strings da nossa url
+    // console.log(req.query)
+
     return res.render("create-point.html")
- })
+})
+
+server.post("/savepoint", (req, res) => {
+
+    //req.body: O corpo do nosso formulário
+    //console.log(req.body)
+
+    //Inserir dados no banco de dados
+    const query = `INSERT INTO places 
+                    (image, name, address, 
+                     address2, state, city, 
+                     items) 
+                    VALUES 
+                    (?, ?, ?, ?, ?, ?, ? );`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso.")
+        console.log(this)
+
+        return res.send("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
 
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    //Pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+        //mostrar a página html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total })
+    })
+
 })
 
 //Ligar o Servidor
